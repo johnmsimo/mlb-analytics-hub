e64d122221616a8dff5c51c77d370c3a85d0da89
johnmsimo <johnmsimo@gmail.com>
2026-04-05 20:58:31 -0400

    fix: use Eastern Time for date string generation

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ ET = ZoneInfo("America/New_York")
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+datetime.now(ET).strftime("%Y-%m-%d")
+datetime.now(ET).strftime('%Y-%m-%d')
+datetime.now(ET).date()
+
 app = Flask(__name__)
 CORS(app)
 
